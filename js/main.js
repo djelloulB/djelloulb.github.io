a881554d73264d688675a0db89351046edfa2dc0
dbcrea <djelloul.be@gmail.com>
2021-06-20 18:40:07 +0200

    adding project and js code for gallery

--- js/main.js
+++ js/main.js
@@ -1,22 +1,22 @@
-// Get the modal
-var modal = document.querySelectorAll("#realisations div.modal");
+"use strict";
+let galImages = document.querySelectorAll('#realisations img');
 
-// Get the image and insert it inside the modal - use its "alt" text as a caption
-//var img = document.getElementById('myImg');
-var img = document.getElementsByClassName('myImg');
-var modalImg = document.getElementById("img01");
-var captionText = document.querySelectorAll("#realisations .caption");
-// Fonction onclick
-img.onclick = function(){
-  modal.style.display = "block";
-  modalImg.src = this.src;
-  captionText.innerHTML = this.alt;
+for(let i = 0; i < galImages.length; i++){
+    let image = galImages[i];
+    image.addEventListener('click', showSinglePict, false);
 }
 
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+function showSinglePict(e){
+    let image = e.target;
+    let imageContainer = document.getElementById('galleryContainer');
+    let bigImage = imageContainer.querySelector('img');
+    bigImage.src = image.src;
+    imageContainer.classList.toggle('visible');
+    imageContainer.addEventListener('click', closeSinglePict, false);
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() { 
-  modal.style.display = "none";
+}
+
+function closeSinglePict(){
+    let imageContainer = document.getElementById('galleryContainer');
+    imageContainer.classList.toggle('visible');
 }
